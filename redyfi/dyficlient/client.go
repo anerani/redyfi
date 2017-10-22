b6e70e80f25dcbd227e47c6a6b1c6dc2e57310fd
Tomi Juntunen <erani@iki.fi>
2017-10-23 01:26:06 +0300

    Move client and dy.fi API access logic to separate package as methods.

---
+++ redyfi/dyficlient/client.go
@@ -0,0 +1,85 @@
+package dyficlient
+
+import (
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"regexp"
+)
+
+var (
+	checkIPURL      = "http://checkip.dy.fi/"
+	updateIPBaseURL = "https://www.dy.fi/nic/update?hostname="
+)
+
+// CheckIP gets the current IP address using the dy.fi service.
+func CheckIP() []byte {
+	response, err := http.Get(checkIPURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// catch only the IP address from the result string. response body format:
+	// Current IP Address: [0-255].[0-255].[0-255].[0-255]
+	IPMatcher, err := regexp.Compile(`\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	IPAddr := IPMatcher.Find(body)
+	if IPAddr == nil {
+		log.Fatal(err)
+	}
+
+	// validate the IP address
+	if net.ParseIP(string(IPAddr)) == nil {
+		log.Fatal(err)
+	}
+
+	return IPAddr
+}
+
+// UpdateIP sends a refresh request to the dy.fi server to update current IP address
+// pointing to a hostname. Returns the response body and status as a string.
+func UpdateIP(username *string, password *string, hostname *string, email *string) ([]byte, string) {
+	updateIPURL := updateIPBaseURL + *hostname
+
+	log.Printf(updateIPURL + "\n")
+	request, err := http.NewRequest("GET", updateIPURL, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request.SetBasicAuth(*username, *password)
+	request.Header.Set("User-Agent", "redyfi/0.0.2 ("+*email+")")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return body, response.Status
+}
--- redyfi/main.go
+++ redyfi/main.go
@@ -2,89 +2,26 @@ package main
 
 import (
 	"flag"
-	"io/ioutil"
 	"log"
-	"net"
-	"net/http"
-	"regexp"
-)
 
-var (
-	checkIPURL      = "http://checkip.dy.fi/"
-	updateIPBaseURL = "https://www.dy.fi/nic/update?hostname="
+	"github.com/anerani/redyfi/dyficlient"
 )
 
 func main() {
 
-	userName := flag.String("username", "", "dy.fi username")
-	passWord := flag.String("password", "", "dy.fi password")
-	hostName := flag.String("hostname", "", "hostname to update")
-	eMail := flag.String("mail", "", "email address for user agent header")
+	username := flag.String("username", "", "dy.fi username")
+	password := flag.String("password", "", "dy.fi password")
+	hostname := flag.String("hostname", "", "hostname to update")
+	email := flag.String("mail", "", "email address for user agent header")
 	flag.Parse()
 
-	response, err := http.Get(checkIPURL)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer response.Body.Close()
-
-	body, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// catch only the IP address from the result string. response body format:
-	// Current IP Address: [0-255].[0-255].[0-255].[0-255]
-	IPMatcher, err := regexp.Compile(`\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	IPAddr := IPMatcher.Find(body)
-	if IPAddr == nil {
-		log.Fatal(err)
-	}
-
-	// validate the IP address
-	if net.ParseIP(string(IPAddr)) == nil {
-		log.Fatal(err)
-	}
+	IPAddr := dyficlient.CheckIP()
 
 	log.Printf("Seems like current IP address is: %s\n", IPAddr)
 	log.Printf("Attempting to update...")
 
-	updateIPURL := updateIPBaseURL + *hostName
+	responseBody, responseStatus := dyficlient.UpdateIP(username, password, hostname, email)
 
-	log.Printf(updateIPURL + "\n")
-	request, err := http.NewRequest("GET", updateIPURL, nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	request.SetBasicAuth(*userName, *passWord)
-	request.Header.Set("User-Agent", "redyfi/0.0.1 ("+*eMail+")")
-
-	client := &http.Client{}
-
-	response, err = client.Do(request)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer response.Body.Close()
-
-	body, err = ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("Response status: %s\n", response.Status)
-	log.Printf("Response body: %s\n", body)
+	log.Printf("Response status: %s\n", responseStatus)
+	log.Printf("Response body: %s\n", responseBody)
 }
