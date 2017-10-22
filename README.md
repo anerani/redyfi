a7d76b2b88685f1ee0faeaf6ee4269f90d502215
Tomi Juntunen <erani@iki.fi>
2017-10-23 00:14:42 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# redyfi - dy.fi IP refresher
+
+Simple app to update [dy.fi](https://www.dy.fi) DynDNS IP address for a hostname.
+
+### Install
+
+Clone this repository and install by running ```go install```.
+
+### Usage
+
+```
+[path]:
+  -hostname string
+        hostname to update
+  -mail string
+        email address for user agent header
+  -password string
+        dy.fi password
+  -username string
+        dy.fi username
+```
---
+++ redyfi/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+func main() {
+
+	userName := flag.String("username", "", "dy.fi username")
+	passWord := flag.String("password", "", "dy.fi password")
+	hostName := flag.String("hostname", "", "hostname to update")
+	eMail := flag.String("mail", "", "email address for user agent header")
+	flag.Parse()
+
+	checkIPURL := "http://checkip.dy.fi/"
+
+	response, err := http.Get(checkIPURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	IPMatcher, err := regexp.Compile(`\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	IPAddr := IPMatcher.Find(body)
+
+	if IPAddr == nil {
+		log.Fatal(err)
+
+	}
+	log.Printf("Seems like current IP address is: %s\n", IPAddr)
+	log.Printf("Attempting to update...")
+
+	updateIPURL := "https://www.dy.fi/nic/update?hostname=" + *hostName
+
+	log.Printf(updateIPURL + "\n")
+	request, err := http.NewRequest("GET", updateIPURL, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request.SetBasicAuth(*userName, *passWord)
+	request.Header.Set("User-Agent", "dyfi-refresh/0.0.1 ("+*eMail+")")
+
+	client := &http.Client{}
+
+	response, err = client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err = ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Response status: %s\n", response.Status)
+	log.Printf("Response body: %s\n", body)
+}
